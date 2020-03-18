2e8aee94bec9e27899158cdcd0f289ad2bae89bd
Kyle Michael Deguzman <kylemichaeldeguzman@kyles-mbp.home>
2020-03-18 14:27:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+ y Git Project 
+
+> My code for the git project
+
+## I'm in the feature branch
+
+
+> Added to master again
---
+++ my_file.py
@@ -0,0 +1,10 @@
+print('Hi Git')
+
+print('Hi from another dev')
+
+def hi_there():
+<<<<<<< HEAD
+	print("hi")
+=======
+  print("hi")
+>>>>>>> ba3dd7988d5a24988a86f76adccd03121bdfb0a5
