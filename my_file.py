b18b9132b45aad9150acb02fedce1abd8105655d
Kyle Michael Deguzman <kylemichaeldeguzman@kyles-mbp.home>
2020-03-18 17:35:41 -0700

    Added new python function

--- my_file.py
+++ my_file.py
@@ -3,8 +3,8 @@ print('Hi Git')
 print('Hi from another dev')
 
 def hi_there():
-<<<<<<< HEAD
 	print("hi")
-=======
-  print("hi")
->>>>>>> ba3dd7988d5a24988a86f76adccd03121bdfb0a5
+
+def hi_there():
+	print("hi")
+
